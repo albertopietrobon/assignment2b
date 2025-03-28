e82e6ec8323e13308b96b4a8193ed44169a0d782
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-03-28 15:54:11 +0000

    c2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,31 @@ import streamlit as st
 
 st.title(":green[Give us your feedback] :pencil:")
 st.write("> Please fill the form below. **Your feedback is really important for us**.")
-form = st.button("Click here for the form")
 
-if form:
-    st.switch_page("pages/form.py")
+with st.form("**USER EXPERIENCE FORM**"):
+
+    name = st.text_input("Full name:", placeholder="Enter you name...")
+    rate = st.slider("Rate your satisfaction:",1,5,3)
+    comment = st.text_area("Additional comments:", placeholder="Write your comments...")
+    
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+
+    if name:
+       if comment:
+           st.write(f"**:green[Form summary:]**\n\n>*User:* {name}\n\n>*Satisfaction:* {rate}\n\n>*Comment:* {comment}")
+           if rate<=2:
+               st.warning("We are really sorry you didn't like the experience :cry:")
+           if rate>=4:
+               st.success("We appreciated a lot your feedback! :smile:")
+
+       else:
+           st.write(f"**:green[Form summary:]**\n\n>*User:* {name}\n\n>*Satisfaction:* {rate}\n\n>*Comment:* No further comment")
+    
+    else:
+        st.warning("Missing name! :warning:")
+
+
+
 
